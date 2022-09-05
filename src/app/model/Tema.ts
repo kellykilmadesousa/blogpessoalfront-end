7f5b4d90f99f512c593f216038d212d563b787fb
Kelly Kilma de Sousa <kellykilmadesousa@gmail.com>
2022-09-05 17:23:46 -0300

    componente inicio sem consumo

--- src/app/model/Tema.ts
+++ src/app/model/Tema.ts
@@ -1,8 +1,7 @@
-import { Postagem } from "./Postagem"
+import { Postagem } from "./Postagem";
 
 export class Tema{
     public id: number
     public descricao: string
     public postagem: Postagem[]
-
 }
